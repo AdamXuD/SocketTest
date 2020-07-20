1113cbb27e3190cf47cc6ab85186d916eff6c650
Gitea <gitea@fake.local>
2020-07-21 01:35:19 +0800

    完成了content与map互相转化的函数

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <cstring>
 #include <string>
+#include <map>
 #include "include/json/json.h"
 
 #define PACKET_HEADER "PACKET_HEADER"
@@ -57,6 +58,34 @@ struct Msg //信息类
         this->fromUser = root["fromUser"].asString();
         this->content = root["content"].asString();
     }
+
+    void mapToContent(string column[2], map<string, string> &pMap)
+    {
+        Json::Value root;
+        map<string, string>::iterator it = pMap.begin();
+        for(int i = 0; i < pMap.size(); i++, it++)
+        {
+            root[i][column[0]] = Json::Value(it->first);
+            root[i][column[1]] = Json::Value(it->second);
+        }
+        this->content = root.toStyledString();
+    }
+
+    void contentToMap(string column[2], map<string,  string> &pMap)
+    {
+        istringstream iss(this->content);
+        Json::CharReaderBuilder rbuilder;
+        string err;
+        Json::Value root;
+        Json::parseFromStream(rbuilder, iss, &root, &err);
+        for(int i = 0; i < root.size(); i++)
+        {
+            cout << root[i][column[0]].asString() << root[i][column[1]].asString() << endl;
+            pMap[root[i][column[0]].asString()] = root[i][column[1]].asString();
+        }
+    }
+
+    
 };
 
 string packUp(string jsonString)
@@ -81,14 +110,42 @@ string unPack(string packet)
 
 int main()
 {
-    Msg test1(2, "儿子", "爸爸", "我是你爸爸！");
-    Msg test2;
-    test2.parser(unPack(packUp(test1.serializer())));
+    // Msg test1(2, "儿子", "爸爸", "我是你爸爸！");
+    // Msg test2;
+    // test2.parser(unPack(packUp(test1.serializer())));
 
-    cout << test2.type << endl;
-    cout << test2.fromUser << endl;
-    cout << test2.toUser << endl;
-    cout << test2.content << endl;
+    // cout << test2.type << endl;
+    // cout << test2.fromUser << endl;
+    // cout << test2.toUser << endl;
+    // cout << test2.content << endl;
 
+    // Json::Value root;
+    // Json::Value element;
+    // element["aaa"] = Json::Value("bbb");
+    // element["ccc"] = Json::Value("ddd");
+    // root["test"][0] = Json::Value(element);
+    // root["test"][1] = Json::Value("aaa");
+    // root["test"][2] = Json::Value("bbb");
+    // cout << root.toStyledString() << endl;
+
+    map<string, string> test;
+    test["aaa"] = "bbb";
+    test["ccc"] = "ddd";
+
+    Msg msg;
+    string column[2] = {"111", "222"};
+    msg.mapToContent(column, test);
+    cout << msg.content << endl;
+
+    Msg msg2;
+    msg2.parser(unPack(packUp(msg.serializer())));
+
+    map<string, string> test2;
+    msg2.contentToMap(column, test2);
+    map<string, string>::iterator it = test2.begin();
+    for(int i = 0; i < test2.size(); i++, it++)
+    {
+        cout << it->first << " " << it->second << endl;
+    }
     return 0;
 }
